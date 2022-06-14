d9ea84d7a0eaab143ec12ffd84ae26ec2d6f1997
jihnkim <sysanal1697@gmail.com>
2022-06-15 07:01:12 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello C#");
+
+            Console.WriteLine(args.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
