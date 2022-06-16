2bf30415b2ddca053942f3a19316c0956697568b
jihnkim <sysanal1697@gmail.com>
2022-06-16 23:34:30 +0900

    forms

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿using System;
+namespace csharp;
 
-namespace csharp
+static class Program
 {
-    class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hello C#");
-
-            Console.WriteLine(args.Length);
-
-            Console.ReadKey();
-        }
-    }
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
 }
